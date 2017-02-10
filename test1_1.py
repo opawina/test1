9c8dedad246eba6e1dbed84be1680474b2136fff
Unknown <ztar4e@yandex.ru>
2017-02-10 11:41:56 +0300

    Success1.1

--- test1_1.py
+++ test1_1.py
@@ -7,44 +7,34 @@ conn = psycopg2.connect('''host = 'localhost'
                            dbname = 'kolxoz'
                            user = 'postgres'
                            password = 'deloper' ''')
-# осоновной запрос к БД
-df_dirty = pd.read_sql('''SELECT calendar.year,
-                           dim_terr.territory_name,
-                           dim_terr.id,
-                           fact.value_additive
-                    FROM public.calendar, public.dim_terr, public.fact
-                    WHERE calendar.date = fact.id_date AND
-                           dim_terr.id = fact.id_terr AND
-                           fact.id_pokazatel = 106 AND
-                           (calendar.year between 2005 and 2015) ''', conn)
+# запрос к БД
+df_dirty = pd.read_sql('''SELECT  dt.id, dt.territory_name, f.value_additive, c.year
+                          FROM calendar c
+                          JOIN fact f ON c.date = f.id_date
+                          RIGHT OUTER JOIN dim_terr dt ON dt.id = f.id_terr
+                          AND f.id_pokazatel = 106
+                          AND c.year > 2004 ''', conn)
 
-# пишлось делать ещё один запрос к БД т.к. названия регионов без показаелей по продукту ->
-# не выгрузились в основном запросе к БД
-df_region = pd.read_sql('''SELECT dim_terr.territory_name, dim_terr.id FROM public.dim_terr;''', conn)
-df_region.set_index('id', inplace=True)
+conn.close()
 
 # создаю ДФ
-df_res = pd.DataFrame(columns=('Территория', '2005-2010', '2011-2015'), index=np.arange(1, 18))
-# вписываю в ДФ регионы и определяю тип ячеек для арифмеических операций
-num_reg = np.arange(1, 18)
-for i in num_reg:
-    q = df_region['territory_name'][i]
-    df_res['Территория'][i] = q
-    df_res['2005-2010'][i] = 0
-    df_res['2011-2015'][i] = 0
+df_res = pd.DataFrame(data=[(0, 0, 0)], columns=('Территория', '2005-2010', '2011-2015'), index=np.arange(1, 18))
 
-# закидываю данные о количестве продукта в ДФ, с сортировкой по периуду времени
+# закидываю названия регионов и количество продукта в ДФ, с сортировкой по периуду времени
 num_time = np.arange(2005, 2011)
 for row in df_dirty.index:
-    q = df_dirty.loc[row]
-    if q['year'] in num_time:
-        df_res['2005-2010'][q['id']] += q['value_additive']
+    cur_row = df_dirty.loc[row]
+    # закидываю названия регионов
+    name_reg = df_dirty['territory_name'][row]
+    df_res.loc[cur_row['id'], 'Территория'] = name_reg
+    # закидываю данные
+    if cur_row['year'] in num_time:
+        df_res.loc[cur_row['id'], '2005-2010'] += cur_row['value_additive']
     else:
-        df_res['2011-2015'][q['id']] += q['value_additive']
+        df_res.loc[cur_row['id'], '2011-2015'] += cur_row['value_additive']
 
 # заменяю нули на сообщение
-df_res = df_res.replace(to_replace=[0], value=['Нет данных'])
-# print(df_res)
-
+df_res = df_res.replace([0, None], ['Нет данных', 'Нет данных'])
+print(df_res)
 # выгружаю ДФ в csv
-df_res.to_csv('ethanol_stat.csv', index=False)
+df_res.to_csv('ethanol_stat.csv', float_format='%g', index=False)
